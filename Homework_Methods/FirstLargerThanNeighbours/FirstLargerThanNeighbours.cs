d7f010a45295c718ff19269d7e498ad0d5751980
psdimitrov <pl.st.dimitrov@gmail.com>
2015-10-01 17:56:59 +0300

    2. Methods-Homework
    Homework for the second lection of Advanced C# course - Methods

---
+++ Homework_Methods/FirstLargerThanNeighbours/FirstLargerThanNeighbours.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class FirstLargerThanNeighbours
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 1, 1, 2, 3, 4, 4 };
+
+        Console.WriteLine(GetIndexOfFirstLargerThanNeighbours(numbers));
+    }
+    static int GetIndexOfFirstLargerThanNeighbours(int[] numbers)
+    {
+        if (numbers[0] > numbers[1])
+        {
+            return 0;
+        }
+        else
+        {
+            for (int i = 1; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] > numbers[i - 1] && numbers[i] > numbers[i + 1])
+                {
+                    return i;
+                }
+            }
+            if (numbers[numbers.Length - 1] > numbers[numbers.Length - 2])
+            {
+                return numbers.Length - 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
+
---
+++ Homework_Methods/LastDigit/LastDigit.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class LastDigit
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(GetLastDigit(n));
+    }
+    static string GetLastDigit(int number)
+    {
+        int digit = number % 10;
+        string lastDigit ="";
+        switch(digit)
+        {
+            case 1:
+                lastDigit = "one";
+                break;
+            case 2:
+                lastDigit = "two";
+                break;
+            case 3:
+                lastDigit = "three";
+                break;
+            case 4:
+                lastDigit = "four";
+                break;
+            case 5:
+                lastDigit = "five";
+                break;
+            case 6:
+                lastDigit = "six";
+                break;
+            case 7:
+                lastDigit = "seven";
+                break;
+            case 8:
+                lastDigit = "eight";
+                break;
+            case 9:
+                lastDigit = "nine";
+                break;
+            case 0:
+                lastDigit = "zero";
+                break;
+        }
+        return lastDigit;
+    }
+}

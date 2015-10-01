9a55695af3570cfdd54a07fa2f4fe206da184c13
psdimitrov <pl.st.dimitrov@gmail.com>
2015-10-01 17:40:39 +0300

    1. Arrays-Lists-Stacks-Homework
    C# Advanced homework-1.Arrays-Lists-stacks

---
+++ Homework1/SelectionSort/SelectionSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+class SelectionSort
+{
+    static void Main()
+    {
+        int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        SelectSort(array);
+
+        Console.WriteLine(string.Join(", ",array));
+    }
+
+    private static void SelectSort(int[] array)
+    {
+        int temp;
+        int min;
+        int index;
+        
+        for (int i = 0; i < array.Length; i++)
+        {
+            min = array[i];
+            index = i;            
+
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[j] < min)
+                {
+                    min = array[j];
+                    index = j;
+                }
+            }
+
+            temp = array[i];
+            array[i] = array[index];
+            array[index] = temp;
+
+        }
+    }
+}
+
